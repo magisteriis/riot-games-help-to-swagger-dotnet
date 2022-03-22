8c35ee6844e03753587ff549a6558f5debd093f7
Mikael Dúi Bolinder <github@bolinder.uk>
2022-03-22 09:27:42 +0100

    Changed tag order

--- RiotGames.Help2Swagger/Program.cs
+++ RiotGames.Help2Swagger/Program.cs
@@ -188,8 +188,8 @@ foreach (var urlFunctions in httpFunctionsByUrl.OrderBy(g => g.Key))
 openApi.Tags = openApi.Paths
     .SelectMany(p => p.Value.Operations)
     .SelectMany(o => o.Value.Tags)
-    .OrderBy(t => !t.Name.StartsWith("Plugin"))
-    .ThenBy(t => t.Name.StartsWith("Plugin Manager"))
+    .OrderBy(t => t.Name.StartsWith("Plugin"))
+    .ThenBy(t => !t.Name.StartsWith("Plugin Manager"))
     .ThenBy(t => t.Name)
     .ToList();
 
